79245f44080308262deace2ca3abd21dbe752169
Abhishek kumar <abhishekfdd@gmail.com>
2018-04-01 17:44:45 +0530

    simple product archive ajax fix

--- includes/class-abwc-ajax-cart-loader.php
+++ includes/class-abwc-ajax-cart-loader.php
@@ -129,6 +129,12 @@ class ABWC_Ajax_Cart_Loader {
 							$product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
 							$product->supports( 'ajax_add_to_cart' ) ? 'ajax_add_to_cart' : '',
 						) ) ),
+							'attributes' => array(
+							'data-product_id'  => $product->get_id(),
+							'data-product_sku' => $product->get_sku(),
+							'aria-label'       => $product->add_to_cart_description(),
+							'rel'              => 'nofollow',
+						),
 					);
 
 					$args = apply_filters( 'woocommerce_loop_add_to_cart_args', wp_parse_args( $args, $defaults ), $product );
